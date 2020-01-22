fa0670853ecd1fd0821efba4ed547794fd5805f8
Mike Avdeev <avdeev2033@icloud.com>
2020-01-22 14:40:50 +0300

    setup: realize weather forecast

--- src/components/App.js
+++ src/components/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import WeatherDisplay from './WeatherDisplay';
+import cities from '../cities';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );// dfd
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      activePlace: 0
+    };
+  }
+  render() {
+    const activePlace = this.state.activePlace;
+    return (
+      <div className="App">
+        {cities.map((place, index) => (
+          <button
+            key={index}
+            onClick={() => {
+              this.setState({ activePlace: index });
+            }}
+          >
+            {place.name}
+          </button>
+        ))}
+        <WeatherDisplay key={activePlace} zip={cities[activePlace].zip} />
+      </div>
+    );
+  }
 }
 
 export default App;
