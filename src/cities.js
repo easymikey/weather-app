9843fac76dedd11e774cb619df0a1dc5eefaa734
Mike Avdeev <avdeev2033@icloud.com>
2020-01-22 13:18:20 +0300

    setup: add cityes to weather forecast

---
+++ src/cities.js
@@ -0,0 +1,8 @@
+const cities = [
+    { name: "Moscow", zip: "101000" },
+    { name: "Saint-Petersburg", zip: "187021" },
+    { name: "Madrid", zip: "28080" },
+    { name: "Munich", zip: "80331" },
+  ];
+
+  export default cities;
